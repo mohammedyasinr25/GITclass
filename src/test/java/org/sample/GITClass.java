3fcd7bca435cf0dc4be3e71de09ce5afa970dc54
mohammedyasinr25 <javaseleniumdummy@gmail.com>
2023-04-05 11:13:14 +0530

    First time push

---
+++ src/test/java/org/sample/GITClass.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+public class GITClass {
+	
+	private void sectionA() {
+
+System.out.println("1");
+
+	}
+	
+	
+	private void sectionB() {
+		System.out.println("2");
+	}
+	
+	
+	private void sectionC() {
+		System.out.println("3");
+	}
+
+}
